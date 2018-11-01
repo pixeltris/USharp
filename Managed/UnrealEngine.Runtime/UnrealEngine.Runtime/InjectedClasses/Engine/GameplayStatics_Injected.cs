1bfd6fa022e8ec22deca020cf22d6e1116999d7f
Tanner555 <theman989898@gmail.com>
2018-11-01 06:24:49 -0700

    Revert "Revert "Attempt To Add GetAllActorsOfClass Extension Method For UGameplayStatics""
    This reverts commit 1eda5c80a2e2789c2df0982ec505599bcc687a51.

---
+++ Managed/UnrealEngine.Runtime/UnrealEngine.Runtime/InjectedClasses/Engine/GameplayStatics_Injected.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using UnrealEngine.Runtime;
+using UnrealEngine.Runtime.Native;
+
+namespace UnrealEngine.Engine
+{
+    public partial class UGameplayStatics : UBlueprintFunctionLibrary
+    {
+        public static void GetAllActorsOfClass(UObject WorldContextObject, UClass ActorClass, out List<AActor> OutActors)
+        {
+            List<IntPtr> OutActorAddresses;
+            Native_UGameplayStatics.GetAllActorsOfClass(WorldContextObject.Address, ActorClass.Address, out OutActorAddresses);
+            OutActors = new List<AActor>();
+            foreach (var _address in OutActorAddresses)
+            {
+                OutActors.Add(GCHelper.Find<AActor>(_address));
+            }
+        }
+    }
+}
